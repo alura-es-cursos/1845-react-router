492cf433fc89c124729d76b4a2ee621f3b857be1
HarlandLohora <harland@lohora.com>
2021-08-05 17:53:42 -0300

    clase03 completada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ListaPosts from "../components/ListaPosts"
 
 const Home = () => {
 
@@ -7,14 +8,10 @@ const Home = () => {
       <div className="container">
         <h2 className="title-page">Pet noticias</h2>
       </div>
-      <section className="container flex flex--center">
-      <article className="card post">
-        <h2 className="post-card__title">Baño en perros</h2>
-        <p className="post-card__text">La mayoría de los perros prefieren saltarse la hora del baño, pero el baño juega un papel importante en la salud del pelaje y la piel de su perro, ya que ayuda a mantenerla limpia y libre de suciedad y parásitos. Tener los productos adecuados puede ayudarlo a sentirse más cómodo.</p>
-      </article>
-    </section>
+      <ListaPosts url="/posts"/>
     </main>
   )
 }
 
 export default Home
+
