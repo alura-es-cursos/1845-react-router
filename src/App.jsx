925156c5e1e96ebed2d0c075313c52a3d9933cfc
HarlandLohora <harland@lohora.com>
2021-08-05 15:59:54 -0300

    clase01 completada

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './assets/css/base/base.css'
+import Home from "./pages/Home"
+import Sobre from "./pages/Sobre"
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   
   return (
   <>
-    
+    <Router>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/sobre">
+        <Sobre />
+      </Route>
+    </Router>
   </>
   )
 }
